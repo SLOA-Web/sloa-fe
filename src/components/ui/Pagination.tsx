90c69afd205094225b680706b9afb6c8436b41be
Salman Faraj <salman.faraj06@gmail.com>
2025-09-03 07:01:30 +0530

    Refactor pagination logic to improve start and end page calculations

--- src/components/ui/Pagination.tsx
+++ src/components/ui/Pagination.tsx
@@ -23,12 +23,20 @@ const Pagination = ({ currentPage, totalPages }: PaginationProps) => {
   const renderPageNumbers = () => {
     const pages = [];
     const maxPagesToShow = 5;
-    let startPage = Math.max(1, currentPage - Math.floor(maxPagesToShow / 2));
-    let endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
+    const initialStartPage = Math.max(
+      1,
+      currentPage - Math.floor(maxPagesToShow / 2)
+    );
+    const initialEndPage = Math.min(
+      totalPages,
+      initialStartPage + maxPagesToShow - 1
+    );
 
-    if (endPage - startPage + 1 < maxPagesToShow) {
-      startPage = Math.max(1, endPage - maxPagesToShow + 1);
-    }
+    const startPage =
+      initialEndPage - initialStartPage + 1 < maxPagesToShow
+        ? Math.max(1, initialEndPage - maxPagesToShow + 1)
+        : initialStartPage;
+    const endPage = Math.min(totalPages, startPage + maxPagesToShow - 1);
 
     for (let i = startPage; i <= endPage; i++) {
       pages.push(
