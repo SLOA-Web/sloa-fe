43a094fb33022a132ee0575553639b123290ddb2
Hashim Kalam <hashiimkalam@gmail.com>
2025-09-12 21:14:04 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/membership/Benefits.tsx
+++ src/components/membership/Benefits.tsx
@@ -4,6 +4,7 @@ import { BENEFITS } from "@/data";
 import { useRef, useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -91,25 +92,35 @@ const Benefits: React.FC = () => {
           ref={descRef}
           className="font-poppins text-[16px] 16 lg:mr-24 leading-8 my-4"
         >
-          Become part of the Sri Lanka Orthopaedic Association (SLOA), the premier
-          professional body representing orthopaedic surgeons and musculoskeletal
-          specialists committed to excellence in patient care and medical advancement.
+          Become part of the Sri Lanka Orthopaedic Association (SLOA), the
+          premier professional body representing orthopaedic surgeons and
+          musculoskeletal specialists committed to excellence in patient care
+          and medical advancement.
         </p>
       </div>
 
-      <div className="px-4 md:px-10 lg:px-16 lg:mt-12 grid grid-cols-1 md:grid-cols-3 gap-8">
+      <div className="px-4 md:px-10 lg:px-16 lg:mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {BENEFITS.map((criteria, idx) => (
           <div
             key={idx}
             ref={(el) => {
               if (el) cardsRef.current[idx] = el;
             }}
-            className="bg-white hover:bg-secondary hover:text-white transition-colors duration-300 rounded-lg shadow p-8 cursor-pointer"
+            className="bg-white hover:bg-secondary hover:text-white transition-colors duration-300 rounded-lg shadow p-8 cursor-pointer group"
           >
-            <h3 className="font-roboto text-[24px] mb-4 font-medium text-primary">
+            <h3 className="font-roboto text-[24px] mb-2 font-medium text-primary">
               {criteria.title}
             </h3>
-            <p className="font-poppins text-[16px] leading-6">{criteria.para}</p>
+             <Image
+              src={criteria.vector}
+              alt=""
+              width={96}
+              height={96}
+              className="w-24 h-24 md:w-32 md:h-32 lg:w-52 lg:h-52 mb-2 mx-auto group-hover:brightness-0 group-hover:invert"
+            />
+            <p className="font-poppins text-[16px] leading-6">
+              {criteria.para}
+            </p>
           </div>
         ))}
       </div>
