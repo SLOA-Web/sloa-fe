a4cd7aa104070c57d67d94b37d0fc7a1bc219be5
Pragash Sasitharan <Pragash1001@gmail.com>
2026-01-24 10:43:01 +0530

    Revise ExecutiveNote content for 2026 session
    Updated the ExecutiveNote content to reflect new themes and messages for the Annual Academic Session 2026.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,11 +43,9 @@ export default function Home() {
         <ExecutiveNote
           role="president"
           title="PRESIDENT NOTE"
-          content1={`It is my great privilege to serve as President of the Sri Lanka Orthopaedic Association, the national body representing orthopaedic surgeons and professionals in Sri Lanka. This year, we gather under the theme “Beyond Bones” — a reminder that our work extends beyond surgical techniques and skeletal repair. We are healers, educators, innovators, and collaborators, committed to restoring not only structure but also dignity, function, and quality of life for our patients.`}
-          content2={`At the Association, we continue to strengthen orthopaedic care in Sri Lanka through advanced training, continuing medical education programmes, hands-on workshops, cadaveric dissections, and collaborative seminars. Our mission also embraces the vital contributions of nurses, physiotherapists, occupational therapists, orthotists, and prosthetists, ensuring a multidisciplinary approach to musculoskeletal health.
-We are expanding partnerships with the Ministry of Health and international orthopaedic associations, forging stronger links that elevate orthopaedic practice, education, and research in Sri Lanka. These collaborations bring global expertise home, while also showcasing our unique experiences to the world.
-
-As we move “Beyond Bones,” let us continue to inspire one another — to innovate, to educate, and to serve with compassion. Together, we can shape the future of orthopaedics in Sri Lanka.`}
+          content1={`It is certainly an honour, pleasure and a privilege to be serving as president of SLOA. On behalf of the Sri Lankan Orthopedic Association, warmly welcome you to the Annual Academic Session 2026 to be held from 12th to 14th November, the Hotel Grand Kandyan, in the historic city of Kandy.`}
+          content2={`The theme for the year “Fostering inclusion and collaboration" in the medical sector reflects our collective responsibility to built a healthy community, that is equitable, multidisciplinary and unity in purpose. Kandy with its rich cultural heritage and tranquil surroundings offers an ideal setting for academic engagement and collegial interaction.
+I hope you will also take to enjoy the warmth of Sri Lankan hospitality and unique charm of this beautiful city.`}
         />
       </Suspense>
     </>
